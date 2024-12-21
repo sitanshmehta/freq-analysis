5d9cb457597c85a2412b906405165126a9ecc9c6
sitanshmehta <s65mehta@uwaterloo.ca>
2024-12-21 23:26:19 +0530

    data input class

---
+++ data_input.cpp
@@ -0,0 +1,14 @@
+#include "data_input.h"
+
+//DataInput::DataInput() {}
+
+QVector<float> DataInput::get_data() const
+{
+    QVector<float> data;
+    return data;
+}
+
+DataInput::DataType DataInput::get_data_type() const
+{
+    return DataType::NONE;
+}
